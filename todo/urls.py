b2b4b432a335a89acfc548a5599783066216b4e3
Alan <madybaev@gmail.com>
2021-01-23 21:45:42 +0600

    added todo marked

--- todo/urls.py
+++ todo/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
     path("books/", books_def, name = "books"),
     path("add-todo/", add_todo, name = "add-todo"),
     path("add-book/", add_book, name = "add-book"),
-    path("delete-todo/<id>/", delete_todo, name = "delete-todo")
+    path("delete-todo/<id>/", delete_todo, name = "delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name = "mark-todo"),
 
 
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
